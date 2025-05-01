b49773028be8ddc754657726912100b0b54c494f
Dan Couvillon <dannycouvillon@gmail.com>
2025-04-30 17:11:55 -0700

    Initial commit with working Hello World UI

--- Hello World!/ContentView.swift
+++ Hello World!/ContentView.swift
@@ -6,19 +6,15 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.largeTitle)
+            Button("Tap me") {
+                print("Button tapped!")
+            }
+            .padding()
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
