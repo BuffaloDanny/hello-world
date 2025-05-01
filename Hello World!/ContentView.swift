4c044adbd1595cdc92643821eb5a1d16dcad7a58
Dan Couvillon <dannycouvillon@gmail.com>
2025-04-30 17:01:15 -0700

    Initial Commit

---
+++ Hello World!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello World!
+//
+//  Created by Dan Couvillon on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello World!/Hello_World_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_App.swift
+//  Hello World!
+//
+//  Created by Dan Couvillon on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
